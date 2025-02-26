10bc0495bea3426e133e5adad5a1d26312cd497f
dmulash <Daniel.MulasHernando@nrel.gov>
2025-02-26 15:21:08 -0700

    2021 USD, 2021 procurement

--- ORBIT/manager.py
+++ ORBIT/manager.py
@@ -1775,13 +1775,13 @@ class ProjectManager:
         the keys below should be passed to the 'project_parameters' subdict.
         """
 
-        site_auction = self.project_params.get("site_auction_price", 122698898)
-        site_assessment = self.project_params.get("site_assessment_cost", 61349449)
+        site_auction = self.project_params.get("site_auction_price", 105989395)
+        site_assessment = self.project_params.get("site_assessment_cost", 52994698)
         construction_plan = self.project_params.get(
-            "construction_plan_cost", 1226989
+            "construction_plan_cost", 1059894
         )
         installation_plan = self.project_params.get(
-            "installation_plan_cost", 306747
+            "installation_plan_cost", 264973
         )
 
         return sum(
