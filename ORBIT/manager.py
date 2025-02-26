b0e63925b5beca6714d7ed068f92ec3165ec3647
dmulash <Daniel.MulasHernando@nrel.gov>
2025-02-26 16:47:58 -0700

    2020 USD, procurement 2020

--- ORBIT/manager.py
+++ ORBIT/manager.py
@@ -1774,14 +1774,13 @@ class ProjectManager:
         Returns project related CapEx line items. To override the defaults,
         the keys below should be passed to the 'project_parameters' subdict.
         """
-
-        site_auction = self.project_params.get("site_auction_price", 122698898)
-        site_assessment = self.project_params.get("site_assessment_cost", 61349449)
+        site_auction = self.project_params.get("site_auction_price", 101233584)
+        site_assessment = self.project_params.get("site_assessment_cost", 50616792)
         construction_plan = self.project_params.get(
-            "construction_plan_cost", 1226989
+            "construction_plan_cost", 1012336
         )
         installation_plan = self.project_params.get(
-            "installation_plan_cost", 306747
+            "installation_plan_cost", 253084
         )
 
         return sum(
